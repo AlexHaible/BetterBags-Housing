7bc0c79630752ab4677f215b8abb319f2573ee4e
Alex Haible <52239823+AlexHaible@users.noreply.github.com>
2026-02-28 23:10:56 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,53 @@
+---@class BetterBags: AceAddon
+local addon = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+
+---@class Categories: AceModule
+local categories = addon:GetModule('Categories')
+
+---@class Localization: AceModule
+local L = addon:GetModule('Localization')
+
+---@class Context: AceModule
+local context = addon:GetModule('Context')
+
+local ctx = context:New('BetterBags_Housing_Registration')
+
+categories:CreateCategory(ctx, {
+  name = L:G("Housing: Decor"),
+  save = true,
+  searchCategory = {
+    query = "type = Housing AND subtype = Decor"
+  },
+  note = L:G("Created by BetterBags - Housing"),
+  priority = 1,
+})
+
+categories:CreateCategory(ctx, {
+  name = L:G("Housing: Dyes"),
+  save = true,
+  searchCategory = {
+    query = 'type = Housing AND subtype = "Housing Dye"'
+  },
+  note = L:G("Created by BetterBags - Housing"),
+  priority = 1,
+})
+
+categories:CreateCategory(ctx, {
+  name = L:G("Housing: Lumber"),
+  save = true,
+  searchCategory = {
+    query = 'type = Tradeskill AND subtype = Other AND name %= Lumber'
+  },
+  note = L:G("Created by BetterBags - Housing"),
+  priority = 1,
+})
+
+categories:CreateCategory(ctx, {
+  name = L:G("Housing: Other"),
+  save = true,
+  searchCategory = {
+    query = 'type = Housing AND NOT (subtype = Decor OR subtype = "Housing Dye")'
+  },
+  note = L:G("Created by BetterBags - Housing"),
+  priority = 2,
+})
